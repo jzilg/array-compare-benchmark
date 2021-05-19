acc4b84b39f2afe0f2494cfb7162b4ea4019a877
Ugo <ugogon@hotmail.de>
2021-05-20 01:39:46 +0200

    better input array generation

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
 const get_differences_from_arrays = require('nodejs-array-different')
 const { compareArrays, compareArrays1 } = require('./compareArrays')
 
-const ARRAY_LENGTH = 2000
+const ARRAY_LENGTH = 10000
+
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
 
 const generateArray = () => {
-    return [...Array(ARRAY_LENGTH)]
+    return shuffle([...Array(ARRAY_LENGTH).keys()]);
 }
 
 console.time('Dummy Data generated')
-const array0 = generateArray().map((entry, index) => ARRAY_LENGTH - index)
-const array1 = generateArray().map((entry, index) => ARRAY_LENGTH - index + ARRAY_LENGTH)
+const array0 = generateArray();
+const array1 = generateArray().map((entry, index) => entry + ARRAY_LENGTH/2)
 console.timeEnd('Dummy Data generated')
 
 console.time('get_differences_from_arrays')
-get_differences_from_arrays(array0, array1)
+let res = get_differences_from_arrays(array0, array1);
 console.timeEnd('get_differences_from_arrays')
+console.log(res.inside_ab.length);
+console.log(res.just_in_a.length);
+console.log(res.just_in_b.length);
 
 console.time('compareArrays')
-compareArrays(array0, array1)
+res = compareArrays(array0, array1)
 console.timeEnd('compareArrays')
+console.log(res.inside_ab.length);
+console.log(res.just_in_a.length);
+console.log(res.just_in_b.length);
 
 console.time('compareArrays1')
-compareArrays1(array0, array1)
+res = compareArrays1(array0, array1)
 console.timeEnd('compareArrays1')
+console.log(res.inside_ab.length);
+console.log(res.just_in_a.length);
+console.log(res.just_in_b.length);
